5402755a0f22ff029ccd8347a1acb9616b246d57
David Čepelík <d@dcepelik.cz>
2017-06-19 21:29:57 +0200

    cbordump simplification and cleanup

--- src/cbordump.c
+++ src/cbordump.c
@@ -1,6 +1,8 @@
 /*
  * cbordump:
  * CBOR stream introspection tool
+ *
+ * This utility shall be linked with the die()-ing memory wrappers.
  */
 
 #include "cbor.h"
@@ -19,32 +21,39 @@
 
 
 static struct option longopts[] = {
-	{ "in",			required_argument,	0,	'i' },
-	{ "out",		required_argument,	0,	'o' },
-	{ "passthru",		no_argument,		0, 	'p' },
-	{ "hex-in",		no_argument,		0,	'h' },
-	{ "hex-out",		no_argument,		0,	'H' },
+	{ "output",		required_argument,	0,	'o' },
+	{ "roundtrip",	no_argument,		0, 	'r' },
+	{ 0,			0,					0,	0 },
 };
+
+
+static void usage(char *argv0, int status)
+{
+	fprintf(stderr, "Usage: %s [OPTION]... [FILE]\n\n", argv0);
+	fprintf(stderr, "With no FILE or when FILE is -, read stdin.\n\n");
+	fprintf(stderr, "Options:\n");
+	fprintf(stderr, "\t%-20sWrite output to FILE instead of stdout\n", "-o, --output=FILE");
+	fprintf(stderr, "\t%-20sRead the data and write it back\n", "-r, --roundtrip");
+	exit(status);
+}
 
 
 int main(int argc, char *argv[])
 {
-	char *fn_in;
-	char *fn_out;
+	char *fname_in = "-";
+	char *fname_out = "-";
 	struct nb_buf *nb_buf_in;
 	struct nb_buf *nb_buf_out;
-	struct cbor_stream *cs_in;
-	struct cbor_stream *cs_out;
+	struct cbor_stream *cbor_in;
+	struct cbor_stream *cbor_out;
 	struct cbor_item item;
-	nb_err_t err;
 	char *argv0;
 	int c;
 	int digit_optind;
 	int option_index;
+	nb_err_t err;
 
-	bool do_file_input = false;
-	bool do_file_output = false;
-	bool passthru = false;
+	bool roundtrip = false;
 	bool hex_input = false;
 	bool hex_output = false;
 	
@@ -52,92 +61,56 @@ int main(int argc, char *argv[])
 
 	while ((c = getopt_long(argc, argv, "hHi:o:p", longopts, &option_index)) != -1) {
 		switch (c) {
-		case 0:
-			fprintf(stderr, "longopt\n");
-			break;
-
 		case 'h':
 			hex_input = true;
 			break;
-
 		case 'H':
 			hex_output = true;
 			break;
-
-		case 'i':
-			do_file_input = true;
-			fn_in = optarg;
-			break;
-
 		case 'o':
-			do_file_output = true;
-			fn_out = optarg;
+			fname_out = optarg;
 			break;
-
-		case 'p':
-			passthru = true;
+		case 'L':
+			roundtrip = true;
 			break;
-
 		default:
-			fprintf(stderr, "Usage: %s [--in <input-file>] [--out <output-file>]\n", argv0);
-			return EXIT_FAILURE;
+			usage(argv0, EXIT_FAILURE);
 		}
 
 	}
 
-	nb_buf_in = nb_buf_new();
-	if (!nb_buf_in) {
-		fprintf(stderr, "%s: out of memory\n", argv0);
-		return EXIT_FAILURE;
-	}
+	if (optind < argc)
+		fname_in = argv[optind];
 
-	if (do_file_input)
-		err = nb_buf_open_file(nb_buf_in, fn_in, O_RDONLY, 0);
+	nb_buf_in = nb_buf_new();
+	cbor_in = cbor_stream_new(nb_buf_in);
+
+	nb_buf_out = nb_buf_new();
+	cbor_out = cbor_stream_new(nb_buf_out);
+
+	if (fname_in && strcmp(fname_in, "-") != 0)
+		err = nb_buf_open_file(nb_buf_in, fname_in, O_RDONLY, 0);
 	else
 		err = nb_buf_open_stdin(nb_buf_in);
 
-	if (hex_input)
-		nb_buf_set_read_filter(nb_buf_in, nb_buf_hex_read_filter);
-
 	if (err != NB_ERR_OK) {
-		fprintf(stderr, "%s: cannot open input file\n", argv0);
+		fprintf(stderr, "%s: cannot open input file '%s'\n", argv0, fname_in);
 		return EXIT_FAILURE;
 	}
 
-	cs_in = cbor_stream_new(nb_buf_in);
-	if (!cs_in) {
-		fprintf(stderr, "%s: out of memory\n", argv0);
-		return EXIT_FAILURE;
-	}
-
-	nb_buf_out = nb_buf_new();
-	if (!nb_buf_out) {
-		fprintf(stderr, "%s: out of memory\n", argv0);
-		return EXIT_FAILURE;
-	}
-
-	if (do_file_output)
-		err = nb_buf_open_file(nb_buf_out, fn_out, O_RDWR | O_CREAT | O_TRUNC, 0666);
+	if (fname_out && strcmp(fname_out, "-") != 0)
+		err = nb_buf_open_file(nb_buf_out, fname_out, O_RDWR | O_CREAT | O_TRUNC, 0666);
 	else
 		err = nb_buf_open_stdout(nb_buf_out);
 	
 	if (err != NB_ERR_OK) {
-		fprintf(stderr, "%s: cannot open output file\n", argv0);
+		fprintf(stderr, "%s: cannot open output file '%s'\n", argv0, fname_out);
 		return EXIT_FAILURE;
 	}
 
-	cs_out = cbor_stream_new(nb_buf_out);
-	if (!cs_out) {
-		fprintf(stderr, "%s: out of memory\n", argv0);
-		return EXIT_FAILURE;
-	}
-
-	if (hex_output)
-		nb_buf_set_write_filter(nb_buf_out, nb_buf_hex_write_filter);
-
-	if (!passthru) {
-		if ((err = cbor_stream_dump(cs_in, stdout)) != NB_ERR_OK) {
-			fprintf(stderr, "%s: error: %s\n", argv0, cbor_stream_strerror(cs_in));
+	if (!roundtrip) {
+		if ((err = cbor_stream_dump(cbor_in, stdout)) != NB_ERR_OK) {
+			fprintf(stderr, "%s: error: %s\n", argv0, cbor_stream_strerror(cbor_in));
 			DEBUG_EXPR("%i", err);
 			return 2;
 		}
@@ -145,23 +118,25 @@ int main(int argc, char *argv[])
 	}
 	else {
 		while (!nb_buf_is_eof(nb_buf_in)) {
-			if ((err = cbor_decode_item(cs_in, &item)) != NB_ERR_OK) {
+			if ((err = cbor_decode_item(cbor_in, &item)) != NB_ERR_OK) {
 				fprintf(stderr, "Error decoding item: %s\n",
-					cbor_stream_strerror(cs_in));
+					cbor_stream_strerror(cbor_in));
 				return 2;
 			}
 
-			if ((err = cbor_encode_item(cs_out, &item)) != NB_ERR_OK) {
+			if ((err = cbor_encode_item(cbor_out, &item)) != NB_ERR_OK) {
 				fprintf(stderr, "Error encoding item: %s\n",
-					cbor_stream_strerror(cs_out));
+					cbor_stream_strerror(cbor_out));
 				return 2;
 			}
 		}
 	}
 
 	nb_buf_close(nb_buf_in);
-	cbor_stream_delete(cs_in);
+	cbor_stream_delete(cbor_in);
+
 	nb_buf_close(nb_buf_out);
-	cbor_stream_delete(cs_out);
+	cbor_stream_delete(cbor_out);
+
 	return EXIT_SUCCESS;
 }
